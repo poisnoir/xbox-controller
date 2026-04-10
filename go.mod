79701229849a1ac600c012211793efd50b42bd53
agharibipour <amir.gharibipour@ubisoft.com>
2026-04-10 09:58:04 -0400

    added spine and basic loop

--- go.mod
+++ go.mod
@@ -1,6 +1,26 @@
 // xbox-controller-spine
 module github.com/poisnoir/xbox-controller-spine
 
-go 1.22
+go 1.24.3
 
-require github.com/holoplot/go-evdev v0.0.0-20250804134636-ab1d56a1fe83 // indirect
+require (
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/grandcat/zeroconf v1.0.0 // indirect
+	github.com/holoplot/go-evdev v0.0.0-20250804134636-ab1d56a1fe83 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
+	github.com/klauspost/reedsolomon v1.12.0 // indirect
+	github.com/miekg/dns v1.1.66 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/poisnoir/mad-go v0.0.0-20260213164930-5bea82a451d0 // indirect
+	github.com/poisnoir/spine-go v0.0.0-20260409170147-77cad4c800d6 // indirect
+	github.com/tjfoc/gmsm v1.4.1 // indirect
+	github.com/xtaci/kcp-go/v5 v5.6.70 // indirect
+	golang.org/x/crypto v0.45.0 // indirect
+	golang.org/x/mod v0.25.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
+	golang.org/x/tools v0.34.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,26 +1,106 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/holoplot/go-evdev"
+	"github.com/poisnoir/spine-go"
 )
 
 func main() {
-	// Typically /dev/input/eventX. You can find yours using 'evtest'
-	// or by listing /dev/input/by-id/
+
 	device, err := evdev.Open("/dev/input/event20")
 	if err != nil {
 		panic(err)
 	}
-)
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	ns, err := spine.JointNamespace("rime", "ppap", logger)
+	if err != nil {
+		panic(err)
+	}
+
+	pub, err := spine.NewPublisher[XboxController](ns, "xbox-controller")
+	if err != nil {
+		panic(err)
+	}
+
+	var data XboxController
 
 	for {
+
 		ev, err := device.ReadOne()
 		if err != nil {
 			break
 		}
-		// Type 1 = Button, Type 3 = Absolute (Sticks/Triggers)
-		fmt.Printf("Type: %d, Code: %d, Value: %d\n", ev.Type, ev.Code, ev.Value)
+		updateControllerState(&data, ev)
+
+		pub.Publish(data)
 	}
+}
+
+func updateControllerState(c *XboxController, ev *evdev.InputEvent) {
+	switch ev.Type {
+	case 1: // EV_KEY (Buttons)
+		val := ev.Value == 1 // true if pressed, false if released
+		switch ev.Code {
+		case 304:
+			c.A = val
+		case 305:
+			c.B = val
+		case 307:
+			c.X = val
+		case 308:
+			c.Y = val
+		case 310:
+			c.LB = val
+		case 311:
+			c.RB = val
+		case 314:
+			c.Back = val
+		case 315:
+			c.Start = val
+		case 316:
+			c.Guide = val
+		case 317:
+			c.LSB = val
+		case 318:
+			c.RSB = val
+		}
+
+	case 3: // EV_ABS (Axes, Triggers, D-Pad)
+		switch ev.Code {
+		// Joysticks (with 10% deadzone approx)
+		case 0:
+			c.LeftStick.X = applyDeadzone(ev.Value)
+		case 1:
+			c.LeftStick.Y = applyDeadzone(ev.Value)
+		case 3:
+			c.RightStick.X = applyDeadzone(ev.Value)
+		case 4:
+			c.RightStick.Y = applyDeadzone(ev.Value)
+
+		// Triggers
+		case 2:
+			c.LT = ev.Value
+		case 5:
+			c.RT = ev.Value
+
+		// D-Pad (Hat Axes)
+		case 16: // Hat0X (Left/Right)
+			c.Left = ev.Value == -1
+			c.Right = ev.Value == 1
+		case 17: // Hat0Y (Up/Down)
+			c.Up = ev.Value == -1
+			c.Down = ev.Value == 1
+		}
+	}
+}
+
+func applyDeadzone(val int32) int32 {
+	if val > -2000 && val < 2000 {
+		return 0
+	}
+	return val
 }
